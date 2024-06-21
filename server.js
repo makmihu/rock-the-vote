2dca17ad8cb10f9a39486227e1f992ec46eaf721
MaKayla Mihu <>
2024-06-21 15:29:36 -0600

    fixing port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const { expressjwt } = require('express-jwt')
+const PORT = process.env.PORT || 6700 
 
 app.use(express.json())
 app.use(morgan('dev'))
@@ -25,7 +26,7 @@ app.use((err, req, res, next) => {
     }
     return res.send({errMsg: err.message})
   })
-  
-app.listen(process.env.PORT, () => {
+
+app.listen(PORT, () => {
     console.log('Server on port 6700')
 })
